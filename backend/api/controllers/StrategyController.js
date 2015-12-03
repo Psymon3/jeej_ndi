409c6ce0fbda96f290391a71826277d725aa5e72
Guillaume Nominé <gnomine@ibsciss.com>
2015-12-03 22:15:19 +0100

    UPDATE backend api

---
+++ backend/api/controllers/StrategyController.js
@@ -0,0 +1,11 @@
+/**
+ * StrategyController
+ *
+ * @description :: Server-side logic for managing strategies
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
